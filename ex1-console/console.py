fda06fef7d53e9029a8116849394260e73dde991
laura-nesta <laura.nesta@outlook.fr>
2023-12-11 10:08:58 +0100

    ajout moment journée

--- ex1-console/console.py
+++ ex1-console/console.py
@@ -1,5 +1,32 @@
 # Dit bonjour ou bonsoir selon le moment de la journée et la langue 
-print("Bonjour");
+import datetime
+
+# Obtenir l'heure actuelle
+heure_actuelle = datetime.datetime.now().time()
+
+# Récupérer l'heure sous forme de chiffres
+heure = heure_actuelle.hour
+
+# Déterminer le moment de la journée en fonction de l'heure
+moment_journee = None
+if 5 <= heure < 12:
+    moment_journee = "matin"
+elif 12 <= heure < 18:
+    moment_journee = "après-midi"
+elif 18 <= heure < 22:
+    moment_journee = "soir"
+else:
+    moment_journee = "nuit"
+
+# Afficher le moment de la journée
+print(f"Nous sommes actuellement dans la {moment_journee}")
+
+# En fonction du moment de la journée, saluer en conséquence
+if moment_journee in ["matin", "après-midi"]:
+    print("Bonjour")
+else:
+    print("Bonsoir")
+
 
 # si un palindrôme est saisie, répond "Bien dit!"
 motSaisie = input("saisissez votre mot : "); 
@@ -11,4 +38,9 @@ if(motInverse == motSaisie):
 
 
 # à l'arrêt dit aurevoir selon le moment de la journée
-print("Au revoir")
+if moment_journee in ["matin", "après-midi"]:
+    print("Au revoir, bonne journée")
+elif moment_journee in ["soir"]:
+    print("Au revoir, bonne soirée")
+else:
+    print("Au revoir, bonne nuit")
